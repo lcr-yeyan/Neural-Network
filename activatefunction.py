b5e58c8c80bc354561d858b797747acd76d4c4d5
lcr-yeyan <56009337+lcr-yeyan@users.noreply.github.com>
2024-07-17 15:31:46 +0800

    Update activatefunction.py

--- activatefunction.py
+++ activatefunction.py
@@ -1,8 +1,8 @@
 import math
 
 
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+def sigmoid(k, x):
+    return 1 / (1 + math.exp(-x * k))
 
 
 def relu(x):
@@ -18,3 +18,13 @@ def sign(x):
         return 0
 
 
+def tanh(x):
+    return (math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x))
+
+
+def softplus(x):
+    return math.log10(1 + math.exp(x))
+
+
+def elu(alpha, x):
+    return x if x > 0 else alpha * (math.exp(x) - 1)

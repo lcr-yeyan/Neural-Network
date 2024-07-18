60fcc0e34ba4ca51ca98ac668266142304361737
lcr-yeyan <56009337+lcr-yeyan@users.noreply.github.com>
2024-07-18 14:46:44 +0800

    Update MLPmodel.py

--- MLPmodel.py
+++ MLPmodel.py
@@ -57,3 +57,31 @@ class LogisticRegression:
         self.y_pred = T.argmax(self.p_y_given_x, axis=1)
         self.params = [self.W, self.b]
 
+
+class MLP3:
+    def __init__(self, rng, input, n_in, n_hidden, n_out):
+        self.hiddenLayer = HiddenLayer(
+            rng=rng,
+            input=input,
+            n_in=n_in,
+            n_out=n_hidden,
+            activation=T.tanh
+        )
+        self.logRegressionLayer = LogisticRegression(
+            input=self.hiddenLayer.output,
+            n_in=n_hidden,
+            n_out=n_out
+        )
+        self.L1 = (
+                abs(self.hiddenLayer.W).sum()
+                + abs(self.logRegressionLayer.W).sum()
+        )
+        self.L2_sqr = (
+                (self.hiddenLayer.W ** 2).sum()
+                + (self.logRegressionLayer.W ** 2).sum()
+        )
+        self.negative_log_likelihood = (
+            self.logRegressionLayer.negative_log_likelihood
+        )
+        self.errors = self.logRegressionLayer.errors
+        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
